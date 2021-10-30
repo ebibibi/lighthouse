2af2efb0b0de61960b5ce808bff6018ad27d6c92
Masahiko Ebisuda <ebibibi@gmail.com>
2021-10-30 16:53:59 +0900

    commit

--- delegatescript.ps1
+++ delegatescript.ps1
@@ -1,7 +1,15 @@
+#共同作成者ロールをmebisuda@jbs.comに与える
+$subscriptionName = "委任したいサブスクリプションの名前をここに記載"
+Get-AzureSubscription -SubscriptionName $subscriptionName | Set-AzContext
+New-AzSubscriptionDeployment -Name lighthousetest `
+                 -Location JapanEast `
+                 -TemplateUri https://raw.githubusercontent.com/ebibibi/lighthouse/master/tomebisudajbs_contributor.json `
+                 -Verbose
+
 #共同作成者ロールをebibibi@gmail.comに与える
 $subscriptionName = "委任したいサブスクリプションの名前をここに記載"
 Get-AzureSubscription -SubscriptionName $subscriptionName | Set-AzContext
 New-AzSubscriptionDeployment -Name lighthousetest `
                  -Location JapanEast `
-                 -TemplateUri https://raw.githubusercontent.com/ebibibi/lighthouse/master/contributor.json `
+                 -TemplateUri https://raw.githubusercontent.com/ebibibi/lighthouse/master/toebibibigmail_contributor.json `
                  -Verbose
